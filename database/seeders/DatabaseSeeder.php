8a821822a03d8eb6199e18095d48ce5405e4c893
galangw <nganuwijaya05@gmail.com>
2023-03-22 00:06:01 +0700

    login page add stisla

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\Hash;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory(10)->create();
+        // User::factory(1)->create();
+        \App\Models\User::factory()->create([
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin')
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// Route::get('/test', function () {
+//     return view('auth.layouts.main-login');
+// });
 
 Auth::routes();
 
